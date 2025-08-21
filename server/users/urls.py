453a2a5a5cb6504ea709ff53a7fe4a1c6d30ff29
nasif-muhamed <muhdnasifk@gmail.com>
2025-08-21 18:17:38 +0530

    fix(urls): add missing route for LogoutView

--- server/users/urls.py
+++ server/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, UserProfileView
+from .views import RegisterView, UserProfileView, LogoutView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='user-login'),
     path('refresh-token/', TokenRefreshView.as_view(), name='refresh-token'),
     path('profile/', UserProfileView.as_view(), name='user-profile'),
+    path('logout/', LogoutView.as_view(), name='logout-user'),
 ]
